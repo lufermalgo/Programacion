d9a54ad113d3c1b14fd23bdda797a20974fae9b6
Luis Fernando Maldonado Arango <fmaldonado@summan.com>
2016-07-14 23:00:07 -0500

    pesoEnMarte

--- HTMLyJavaScript/alert.js
+++ HTMLyJavaScript/alert.js
@@ -1,2 +1,10 @@
-// Esto es una alerta en pantalla
-alert("Estas listo para continuar!!!...");
+var	nombre = "Fernando";
+var apellido = "Maldonado";
+
+alert("¿Quieres saber tu peso en Marte?..");
+
+peso = prompt("¿Cual es tu peso en kg?");
+peso = parseInt(peso);
+pesoEnMarte = (peso / 9.81) * 3.711;
+
+alert(nombre + " " + apellido + "\n" + "Tu peso en la tierra es: " + peso + " Kg." + "\n" + "Tu peso en Marte es: " + pesoEnMarte + " Kg.");
